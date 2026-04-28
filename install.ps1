91cc5c5531378300f56a0b25e1a56c338f4db9b1
Christian Arensbak <christian@arensbak.dev>
2026-04-28 15:02:20 +0200

    Fix locale check for setting system language

--- install.ps1
+++ install.ps1
@@ -16,7 +16,7 @@ if (!(Test-Path -Path $stateFile)) {
 ###############################
 if (Select-String -Path $stateFile -Pattern "Language: Fals") {
   Write-Host "Setting system locale to en-US..." -ForegroundColor Cyan
-  if (!(Get-UICulture -eq "en-US")) {
+  if (!((Get-UICulture).Name -eq "en-US")) {
     Install-Language en-US
     Set-WinUserLanguageList -LanguageList en-US -Force
   }

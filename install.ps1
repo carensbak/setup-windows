49583a27972457a98e78cedf4b8f7cd524396fbe
Christian Arensbak <christian@arensbak.dev>
2026-04-28 14:30:46 +0200

    style: Use Write-Host rather than echo to support colors in the terminal

--- install.ps1
+++ install.ps1
@@ -3,23 +3,23 @@
 ###############################
 $stateFile = "$HOME\temp-setup\state.txt"
 if (!(Test-Path -Path $stateFile)) {
-  echo "State file not found, creating one under '$HOME\temp-setup\state.txt'..." -ForegroundColor Cyan
+  Write-Host "State file not found, creating one under '$HOME\temp-setup\state.txt'..." -ForegroundColor Cyan
   New-Item -Path "$HOME\temp-setup" -ItemType directory -Force
   New-Item -Path "$HOME\temp-setup\state.txt" -ItemType file
 
   "Language: False" >> "$HOME\temp\state.txt"
-  echo "✔ State file created" -ForegroundColor Green
+  Write-Host "✔ State file created" -ForegroundColor Green
 }
 
 ###############################
 # English language locale
 ###############################
 if (Select-String -Path $stateFile -Pattern "Language: False") {
-  echo "Setting system locale to en-US..." -ForegroundColor Cyan
+  Write-Host "Setting system locale to en-US..." -ForegroundColor Cyan
   if (!(Get-UICulture -eq "en-US")) {
     Install-Language en-US
     Set-WinUserLanguageList -LanguageList en-US -Force
   }
   (Get-Content $stateFile) -replace "Language: False", "Language: True" | Set-Content $stateFile
-  echo "✔ System locale set to en-US" -ForegroundColor Green
+  Write-Host "✔ System locale set to en-US" -ForegroundColor Green
 }

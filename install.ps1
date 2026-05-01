f5160648f0a23c0f15b5b9dd333c60b73707b752
Christian Arensbak <christian@arensbak.dev>
2026-05-01 10:19:34 +0200

    Fix plugin installation command for VSCode

--- install.ps1
+++ install.ps1
@@ -177,7 +177,9 @@ if (Select-String -Path $stateFile -Pattern "VSCode: False") {
 
    # Install the VSCode plugins
   $pluginsList = irm "https://raw.githubusercontent.com/carensbak/setup-windows/refs/heads/master/vscode-plugins.txt"
-  $pluginslist | ForEach-Object { code --install-extension $_ }
+  $pluginslist -split "`r?`n" | Where-Object { $_.Trim() } | ForEach-Object { 
+    code --install-extension $_
+  }
 
   # Copy over the settings
   $settingsContent = irm "https://raw.githubusercontent.com/carensbak/setup-windows/refs/heads/master/vscode-settings.json"
